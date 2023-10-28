b1897d3f10460cac061ce251086bba741953b894
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-10-28 20:51:05 +0700

    feat: add generate password

---
+++ index.js
@@ -0,0 +1,14 @@
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+function generatePassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+console.log(generatePassword(12)); // Example usage: generates a random password with length of 12 characters
