7c349e280cc352f47218cdfb81997eda9426c7fc
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-10-28 21:03:48 +0700

    update comment syntax

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ function generatePassword(length) {
     return password;
 }
 
-console.log(generatePassword(12)); // Example usage: generates a random password with length of 12 characters
-// Sample user data
+console.log(generatePassword(12)); /* Example usage: generates a random password with length of 12 characters
+/* Sample user data */
 const users = [
     { username: 'user1', password: 'password1' },
     { username: 'user2', password: 'password2' },
     { username: 'user3', password: 'password3' }
 ];
 
-// Function to check if user exists and password is correct
+/* Function to check if user exists and password is correct */
 function login(username, password) {
     const user = users.find(user => user.username === username);
     if (!user) {
@@ -31,7 +31,7 @@ function login(username, password) {
     return 'Login successful';
 }
 
-// Example usage
+/* Example usage */
 console.log(login('user1', 'password1')); // Output: Login successful
 console.log(login('user2', 'wrongpassword')); // Output: Incorrect password
 console.log(login('nonexistentuser', 'password')); // Output: User not found
