449f69d2589bc4bc86090d70f6fe18a9845b8516
Reza Irfan Wijaya <87264553+rezairfanwijaya@users.noreply.github.com>
2023-10-28 21:00:51 +0700

    [FEAT] add generated password (#1)
    * update style
    
    * feat: add generate password
    
    * feat: update height in navbar

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+function generatePassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+console.log(generatePassword(12)); // Example usage: generates a random password with length of 12 characters
 // Sample user data
 const users = [
     { username: 'user1', password: 'password1' },
